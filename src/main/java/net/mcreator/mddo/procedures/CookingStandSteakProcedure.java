79905cb35db517f5f691a7699e4b05f4c9139a1c
CujaonMods <lukac@192.168.1.3>
2024-06-12 10:27:26 +0200

    stone oven

--- src/main/java/net/mcreator/mddo/procedures/CookingStandSteakProcedure.java
+++ src/main/java/net/mcreator/mddo/procedures/CookingStandSteakProcedure.java
@@ -39,5 +39,166 @@ public class CookingStandSteakProcedure {
 				_player.containerMenu.broadcastChanges();
 			}
 		}
+		if (Items.PORKCHOP == (entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt
+				? ((Slot) _slt.get((int) MddoModVariables.MapVariables.get(world).whichSlot)).getItem()
+				: ItemStack.EMPTY).getItem()) {
+			if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+				ItemStack _setstack = new ItemStack(Items.COOKED_PORKCHOP).copy();
+				_setstack.setCount((int) (new Object() {
+					public int getAmount(int sltid) {
+						if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+							ItemStack stack = ((Slot) _slots.get(sltid)).getItem();
+							if (stack != null)
+								return stack.getCount();
+						}
+						return 0;
+					}
+				}.getAmount((int) (MddoModVariables.MapVariables.get(world).whichSlot + 1)) + 1));
+				((Slot) _slots.get((int) (MddoModVariables.MapVariables.get(world).whichSlot + 1))).set(_setstack);
+				_player.containerMenu.broadcastChanges();
+			}
+			if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+				((Slot) _slots.get((int) MddoModVariables.MapVariables.get(world).whichSlot)).remove(1);
+				_player.containerMenu.broadcastChanges();
+			}
+		}
+		if (Items.CHICKEN == (entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt
+				? ((Slot) _slt.get((int) MddoModVariables.MapVariables.get(world).whichSlot)).getItem()
+				: ItemStack.EMPTY).getItem()) {
+			if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+				ItemStack _setstack = new ItemStack(Items.COOKED_CHICKEN).copy();
+				_setstack.setCount((int) (new Object() {
+					public int getAmount(int sltid) {
+						if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+							ItemStack stack = ((Slot) _slots.get(sltid)).getItem();
+							if (stack != null)
+								return stack.getCount();
+						}
+						return 0;
+					}
+				}.getAmount((int) (MddoModVariables.MapVariables.get(world).whichSlot + 1)) + 1));
+				((Slot) _slots.get((int) (MddoModVariables.MapVariables.get(world).whichSlot + 1))).set(_setstack);
+				_player.containerMenu.broadcastChanges();
+			}
+			if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+				((Slot) _slots.get((int) MddoModVariables.MapVariables.get(world).whichSlot)).remove(1);
+				_player.containerMenu.broadcastChanges();
+			}
+		}
+		if (Items.RABBIT == (entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt
+				? ((Slot) _slt.get((int) MddoModVariables.MapVariables.get(world).whichSlot)).getItem()
+				: ItemStack.EMPTY).getItem()) {
+			if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+				ItemStack _setstack = new ItemStack(Items.COOKED_RABBIT).copy();
+				_setstack.setCount((int) (new Object() {
+					public int getAmount(int sltid) {
+						if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+							ItemStack stack = ((Slot) _slots.get(sltid)).getItem();
+							if (stack != null)
+								return stack.getCount();
+						}
+						return 0;
+					}
+				}.getAmount((int) (MddoModVariables.MapVariables.get(world).whichSlot + 1)) + 1));
+				((Slot) _slots.get((int) (MddoModVariables.MapVariables.get(world).whichSlot + 1))).set(_setstack);
+				_player.containerMenu.broadcastChanges();
+			}
+			if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+				((Slot) _slots.get((int) MddoModVariables.MapVariables.get(world).whichSlot)).remove(1);
+				_player.containerMenu.broadcastChanges();
+			}
+		}
+		if (Items.MUTTON == (entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt
+				? ((Slot) _slt.get((int) MddoModVariables.MapVariables.get(world).whichSlot)).getItem()
+				: ItemStack.EMPTY).getItem()) {
+			if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+				ItemStack _setstack = new ItemStack(Items.COOKED_MUTTON).copy();
+				_setstack.setCount((int) (new Object() {
+					public int getAmount(int sltid) {
+						if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+							ItemStack stack = ((Slot) _slots.get(sltid)).getItem();
+							if (stack != null)
+								return stack.getCount();
+						}
+						return 0;
+					}
+				}.getAmount((int) (MddoModVariables.MapVariables.get(world).whichSlot + 1)) + 1));
+				((Slot) _slots.get((int) (MddoModVariables.MapVariables.get(world).whichSlot + 1))).set(_setstack);
+				_player.containerMenu.broadcastChanges();
+			}
+			if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+				((Slot) _slots.get((int) MddoModVariables.MapVariables.get(world).whichSlot)).remove(1);
+				_player.containerMenu.broadcastChanges();
+			}
+		}
+		if (Items.POTATO == (entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt
+				? ((Slot) _slt.get((int) MddoModVariables.MapVariables.get(world).whichSlot)).getItem()
+				: ItemStack.EMPTY).getItem()) {
+			if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+				ItemStack _setstack = new ItemStack(Items.BAKED_POTATO).copy();
+				_setstack.setCount((int) (new Object() {
+					public int getAmount(int sltid) {
+						if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+							ItemStack stack = ((Slot) _slots.get(sltid)).getItem();
+							if (stack != null)
+								return stack.getCount();
+						}
+						return 0;
+					}
+				}.getAmount((int) (MddoModVariables.MapVariables.get(world).whichSlot + 1)) + 1));
+				((Slot) _slots.get((int) (MddoModVariables.MapVariables.get(world).whichSlot + 1))).set(_setstack);
+				_player.containerMenu.broadcastChanges();
+			}
+			if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+				((Slot) _slots.get((int) MddoModVariables.MapVariables.get(world).whichSlot)).remove(1);
+				_player.containerMenu.broadcastChanges();
+			}
+		}
+		if (Items.COD == (entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt
+				? ((Slot) _slt.get((int) MddoModVariables.MapVariables.get(world).whichSlot)).getItem()
+				: ItemStack.EMPTY).getItem()) {
+			if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+				ItemStack _setstack = new ItemStack(Items.COOKED_COD).copy();
+				_setstack.setCount((int) (new Object() {
+					public int getAmount(int sltid) {
+						if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+							ItemStack stack = ((Slot) _slots.get(sltid)).getItem();
+							if (stack != null)
+								return stack.getCount();
+						}
+						return 0;
+					}
+				}.getAmount((int) (MddoModVariables.MapVariables.get(world).whichSlot + 1)) + 1));
+				((Slot) _slots.get((int) (MddoModVariables.MapVariables.get(world).whichSlot + 1))).set(_setstack);
+				_player.containerMenu.broadcastChanges();
+			}
+			if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+				((Slot) _slots.get((int) MddoModVariables.MapVariables.get(world).whichSlot)).remove(1);
+				_player.containerMenu.broadcastChanges();
+			}
+		}
+		if (Items.SALMON == (entity instanceof Player _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt
+				? ((Slot) _slt.get((int) MddoModVariables.MapVariables.get(world).whichSlot)).getItem()
+				: ItemStack.EMPTY).getItem()) {
+			if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+				ItemStack _setstack = new ItemStack(Items.COOKED_SALMON).copy();
+				_setstack.setCount((int) (new Object() {
+					public int getAmount(int sltid) {
+						if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+							ItemStack stack = ((Slot) _slots.get(sltid)).getItem();
+							if (stack != null)
+								return stack.getCount();
+						}
+						return 0;
+					}
+				}.getAmount((int) (MddoModVariables.MapVariables.get(world).whichSlot + 1)) + 1));
+				((Slot) _slots.get((int) (MddoModVariables.MapVariables.get(world).whichSlot + 1))).set(_setstack);
+				_player.containerMenu.broadcastChanges();
+			}
+			if (entity instanceof Player _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+				((Slot) _slots.get((int) MddoModVariables.MapVariables.get(world).whichSlot)).remove(1);
+				_player.containerMenu.broadcastChanges();
+			}
+		}
 	}
 }
